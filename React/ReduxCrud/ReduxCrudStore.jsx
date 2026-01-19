7d13ccfe386ea513b9b06d5f7a188813117a5e67
janki-ops <mulanijanki051@gmail.com>
2026-01-19 14:14:36 +0530

    Delete React/ReduxCrud directory

--- React/ReduxCrud/ReduxCrudStore.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createStore } from 'redux'
-import ReduxCrudReducer from './ReduxCrudReducer'
-
-const ReduxCrudStore = createStore(ReduxCrudReducer)
-
-export default ReduxCrudStore
