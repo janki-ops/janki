1fce6c1ee0c201c316f11c5670b71e7e8e62d335
janki-ops <mulanijanki051@gmail.com>
2026-01-19 14:12:33 +0530

    Add files via upload

---
+++ React/Next.js/app/Menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <div>
+        <a href='/'>Home||</a>
+        <a href='/about'>About||</a>
+        <a href='/contact'>Contact</a>
+    </div>
+  )
+}
---
+++ React/Next.js/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   
+         <>
+         <h3 className="text-sky-500">Welcome To Home Page!!</h3>
+         </>
+  );
+}
